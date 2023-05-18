89fbbb64d18c73c195d9fa359a6b3b3c6d63bcd8
jarvisisika <jarvis.isika@student.moringaschool.com>
2023-05-18 04:48:28 +0300

    completed assignment

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,99 @@
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    return "Access granted"
+  else
+    return "Access denied"
+  end
 end
+
+puts admin_login("sudo", "12345")
+# Output: Access denied
+
+puts admin_login("admin", "12345")
+# Output: Access granted
+
+puts admin_login("ADMIN", "12345")
+# Output: Access granted
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    return "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 65
+    return "It's a little chilly out there!"
+  elsif temperature > 85
+    return "It's too dang hot out there!"
+  else
+    return "It's perfect out there!"
+  end
 end
 
-def fizzbuzz(num)
-  # your code here
+puts hows_the_weather(33)
+# Output: It's brisk out there!
+
+puts hows_the_weather(99)
+# Output: It's too dang hot out there!
+
+puts hows_the_weather(75)
+# Output: It's perfect out there!
+
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    return "FizzBuzz"
+  elsif number % 3 == 0
+    return "Fizz"
+  elsif number % 5 == 0
+    return "Buzz"
+  else
+    return number
+  end
 end
+
+puts fizzbuzz(1)
+# Output: 1
+
+puts fizzbuzz(2)
+# Output: 2
+
+puts fizzbuzz(3)
+# Output: Fizz
+
+puts fizzbuzz(4)
+# Output: 4
+
+puts fizzbuzz(5)
+# Output: Buzz
+
+puts fizzbuzz(15)
+# Output: FizzBuzz
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+"
+    return num1 + num2
+  when "-"
+    return num1 - num2
+  when "*"
+    return num1 * num2
+  when "/"
+    return num1 / num2
+  else
+    puts "Invalid operation!"
+    return nil
+  end
 end
 
+puts calculator("+", 1, 1)
+# Output: 2
+
+puts calculator("-", 3, 1)
+# Output: 2
+
+puts calculator("*", 3, 2)
+# Output: 6
+
+puts calculator("/", 4, 2)
+# Output: 2
+
+puts calculator("nope", 4, 2)
+# Output: Invalid operation!
+# Output: nil
